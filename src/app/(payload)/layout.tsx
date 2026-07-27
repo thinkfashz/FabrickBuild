a895621c0e2d32726d183c7b9db7683930fe7a6c
thinkfashz <faubricioedms@gmail.com>
2026-07-27 01:53:08 -0400

    Align Payload admin layout with v3.86

--- src/app/(payload)/layout.tsx
+++ src/app/(payload)/layout.tsx
@@ -2,28 +2,22 @@
 import config from '@payload-config'
 import '@payloadcms/next/css'
 import type { ServerFunctionClient } from 'payload'
-import {
-  generatePayloadViewport,
-  handleServerFunctions,
-  RootLayout
-} from '@payloadcms/next/layouts'
+import { handleServerFunctions, RootLayout } from '@payloadcms/next/layouts'
 import React from 'react'
 
 import { importMap } from './admin/importMap.js'
 import './custom.css'
 
-export const generateViewport = generatePayloadViewport
-
 type Args = {
   children: React.ReactNode
 }
 
-const serverFunction: ServerFunctionClient = async (args) => {
+const serverFunction: ServerFunctionClient = async function (args) {
   'use server'
   return handleServerFunctions({
     ...args,
     config,
-    importMap
+    importMap,
   })
 }
 
